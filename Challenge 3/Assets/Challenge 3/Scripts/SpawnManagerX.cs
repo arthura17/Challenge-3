cf13d7df7ae92c05cc59bf0117d702e2b3d31f81
younglane <59858521+younglane@users.noreply.github.com>
2021-03-04 21:14:32 -0500

    Randomly spawn bombs or fireworks
    Stops when game is over

--- Challenge 3/Assets/Challenge 3/Scripts/SpawnManagerX.cs
+++ Challenge 3/Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -7,19 +7,32 @@ public class SpawnManagerX : MonoBehaviour
     public GameObject[] objectPrefabs;
     private float spawnDelay = 2;
     private float spawnInterval = 1.5f;
+    private float yPos;
+    private int index;
+
+    private PlayerControllerX playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
         InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
+    }
+
+    private void Update()
+    {
+        yPos = Random.Range(5, 15);
+        index = Random.Range(0, 2);
     }
 
     // Spawn obstacles
     void SpawnObjects()
     {
-        Vector3 spawnLocation = new Vector3(30, 10, 0);
-        int index = 0;
-
-        Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        if (!playerControllerScript.gameOver)
+        {
+            Vector3 spawnLocation = new Vector3(30, yPos, 0);
+            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        }
     }
+       
 }
